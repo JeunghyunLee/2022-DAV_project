455c8842d45506cb5fce0d0b5a22cccf12d2f27b
Nayeonlee0423 <116620150+Nayeonlee0423@users.noreply.github.com>
2022-11-27 23:58:00 +0900

    Add files via upload

--- app_tropical.py
+++ app_tropical.py
@@ -3,81 +3,90 @@ import pandas as pd
 import altair as alt 
 import numpy as np
 
-
 import folium
 from folium.plugins import HeatMap
 from streamlit_folium import st_folium
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import xgboost as xgb
 import time
+import plotly.express as px
 
-# Add a placeholder 진행 상황 바
-latest_iteration = st.empty()
-bar = st.progress(0)
-for i in range(100):
-    # Update the progress bar with each iteration.
-    latest_iteration.text(f'Iteration {i+1}')
-    bar.progress(i + 1)
-    time.sleep(0.01)
+df = pd.read_csv('total.csv')
 
-st.write('# TropicalNight_seoulgyeonggi')
+## region_selectbox
+region_options = df['지역'].unique().tolist()
+region = st.selectbox('Which region would you like to see?', region_options, 0)
+df = df[df['지역'] == region]
+df.drop(['Unnamed: 0'], axis = 1, inplace = True)
+df.drop(['Unnamed: 0.1'], axis = 1, inplace = True)
 
-st.subheader('1973년 ~ 2022년 월별 평균열대야일수')
-
-
-# load data
-df = pd.read_csv('data_tropicalnight/서울경기.csv')
-df.drop(columns='Unnamed: 0', inplace = True)
-df.insert(0, '지역', 'Gyeonggi-do')
-df1 = df[(df['월']=='6월') | (df['월']=='7월') | (df['월']=='8월') | (df['월']=='9월')]
-df1
-
-line_chart = alt.Chart(df1).mark_line(interpolate='basis').encode(
-    alt.X('연도', title='연도'),
-    alt.Y('평균열대야일수', title='평균열대야일수'),
-    color = '월'
+## line_chart animation
+lines = alt.Chart(df).mark_line().encode(
+    x=alt.X('연도', title='연도'),
+    y=alt.Y('연합계', title='연합계'),
+    color = '지역'
 ).properties(
-    title = '서울경기'
+    width=600,
+    height=450
 )
 
-st.altair_chart(line_chart)
+def plot_animation(df):
+    lines = alt.Chart(df).mark_line().encode(
+       x=alt.X('연도', axis=alt.Axis(title='연도')),
+       y=alt.Y('연합계',axis=alt.Axis(title='연합계')),
+     ).properties(
+    width=600,
+    height=450
+)
+    return lines
 
 
-# Interactive widgets in Streamlit
-taxi_mode = st.selectbox("월", ("6월", "7월", "8월", "9월"))
-year = st.slider("연도", 1973, 2022, (1973, 2022))
+N = df.shape[0] # number of elements in the dataframe
+burst = 6       # number of elements (months) to add to the plot
+size = burst     # size of the current dataset
+
+line_plot = st.altair_chart(lines)
+start_btn = st.button('Start')
+
+if start_btn:
+   for i in range(1,N):
+      step_df = df.iloc[0:size]
+      lines = plot_animation(step_df)
+      line_plot = line_plot.altair_chart(lines)
+      size = i + burst
+      if size >= N: 
+         size = N - 1
+      time.sleep(0.1)
 
 
-# 지역을 고르는 select box
-option = st.sidebar.selectbox(
-    '어떤 지역을 고르시겠습니까?',
-    ('서울경기', '강원영동', '강원영서', '충북', "충남", "경북", "경남", "전북", "전남", "제주"))
+st.markdown('#')
+st.markdown('#')
 
 
-# 지도 그림
 
-import json
-geodata = json.load(open('stanford-dk009rq9138-geojson.json', 'r',encoding='utf-8'))
+## region_multiselectbox
+df2 = pd.read_csv('total3.csv')
+region_options = df2['지역'].unique().tolist()
+region = st.multiselect('Which region would you like to see?', region_options, ['강원도'])
+df2 = df2[df2['지역'].isin(region)]
 
+
+# map_animation 
+# 지역 선택 -> 연도별 색깔 변화
 map = folium.Map(location=[36,127], zoom_start=7, scrollWheelZoom = False, tiles='CartoDB positron')
 
 choropleth = folium.Choropleth(
-    geo_data=geodata,
-    data = df,
-    columns=('지역', '평균열대야일수') ,
-    key_on='feature.properties.name_1'
+    geo_data='stanford-dk009rq9138-geojson.json',
+    data = df2,
+    columns=('지역', '연합계') ,
+    key_on='feature.properties.nl_name_1'
 )
 choropleth.geojson.add_to(map)
 
 st_map = st_folium(map, width=500, height=660)
 
-
-
-
-
+      
 if st.checkbox('Show Dataframe'):
     st.subheader('This is my dataset:')
     st.write(df)
-
-
-
