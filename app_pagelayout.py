3e74a3428ca21f6e7b219f63c981d0915d21f1e2
Nayeonlee0423 <116620150+Nayeonlee0423@users.noreply.github.com>
2022-12-02 01:25:29 +0900

    Add files via upload

--- app_pagelayout.py
+++ app_pagelayout.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 import datetime
 import time
+from utilities import to_map_df
+import matplotlib.pyplot as plt
+import time, json
+
+
 
 import streamlit as st
 import seaborn as sns
@@ -34,7 +39,7 @@ with st.sidebar:
     )
 
 # Data Frame
-path = 'C:/Users/chaelin/DAVproj/2022-DAV_project-main/2022-DAV_project-main/data_temperature/'
+path = '/Users/nayeonlee/Documents/GitHub/2022-DAV_project/data_temperature/'
 names = ['강원영동', '강원영서', '경남', '경북',
         '서울경기', '전남', '전북', '제주', '충남', '충북']
 
@@ -270,26 +275,37 @@ if (selected != 'Home') & (add_topic == '기온') :
 # Stats 선택 & 열대야 선택
 elif (selected != 'Home') & (add_topic == '열대야'):
 
+    tropical = pd.read_csv('total.csv')
+
     # 가운데 정렬 - 소제목 설정
     st.markdown(
             '''
         <h3 style=
         '
-        text-align: left;
+        text-align: center;
         color: green;
         font-family:apple;
         ' > Tropical Nights''', unsafe_allow_html=True)
     
     # markdown text로 제목 
-    st.markdown("# Mapping Demo")
+    st.markdown("# 열대야일수")
     
+    
+    # 펼쳐지는 페이지 설정 
+    with st.expander("See explanation"):
+         st.write("""
+            열대야일수는 밤최저기온이 25 ℃ 이상인 날로 정의합니다. 기온이 밤에도 25 ℃ 이하로 내려가지 않을 때에는 너무 더워서 사람이 잠들기 어렵기 때문에 더위를 나타내는 지표로 열대야를 사용합니다.
+        """)
+         st.image("https://t3.ftcdn.net/jpg/02/56/12/92/360_F_256129231_RHUe7uAQGPxUmUnFAtaB5pzYhPNCLCed.jpg")
+        
     filter1, filter2 = st.columns(2)
 
     # 페이지 내에서 지역 multi select
     with filter1 :
-            region_filter = st.multiselect(
-            "Choose regions", df['지역'].unique().tolist(), 
+            region_filter = st.selectbox(
+            "Choose regions", tropical['location'].unique().tolist(), 
         )
+            
     # 페이지 내에서 year 선택
     with filter2 :
         year_slider = st.slider(
@@ -304,19 +320,12 @@ elif (selected != 'Home') & (add_topic == '열대야'):
     #         1973, 2021, (1980))
     #     st.write('Selected Year:', year_slider)
 
-    # 펼쳐지는 페이지 설정 
-    with st.expander("See explanation"):
-        st.write("""
-            Tropical Night is when ...
-        """)
-        st.image("https://t3.ftcdn.net/jpg/02/56/12/92/360_F_256129231_RHUe7uAQGPxUmUnFAtaB5pzYhPNCLCed.jpg")
-
+ 
     # 데이터 정보 요약 표현 가능한 metrics
 
     st.write('### Region Statistics')
-    region_filter = region_filter[0]
-    df_filtered = df[(df['지역'] == region_filter) & (df['year'] == year_slider) ]
-    df_filtered_cityonly = df[(df['지역'] == region_filter)]
+    tropical_filtered = tropical[(tropical['location'] == region_filter) & (tropical['year'] == year_slider)]
+    tropical_filtered_cityonly = tropical[(tropical['location'] == region_filter)]
 
     kpi1, kpi2, kpi3, kpi4 = st.columns(4)
 
@@ -324,36 +333,150 @@ elif (selected != 'Home') & (add_topic == '열대야'):
         label=f"You are now at",
         value=region_filter,
     )
-
     kpi2.metric(
-        label=f"Average summer temp of this region on {year_slider} ⏳",
+        label=f"Average number of tropical nights⏳",
         value=round(
-            df_filtered[df_filtered['month'].isin([6, 7, 8])]
-            ['avg'].mean()
+            tropical_filtered['data'].mean()
             ),
         # delta=round(df_filtered['avg'].mean()) - 10,
     )
 
-    lowestyear = df_filtered_cityonly[(df_filtered_cityonly['month'].isin([12, 1, 2]))].groupby(by = 'year').mean().reset_index()
-    lowestyear = lowestyear.sort_values(by = 'min', ascending = True)[['year', 'min']].iloc[0,:]
+    
+    lowestyear = tropical_filtered_cityonly.sort_values(by = 'data', ascending = True)[['year', 'data']].iloc[0,:]
+
 
     kpi3.metric(
         label="Coldest year 🥶",
         value= lowestyear[0],
-        delta= 'temp: '+ str(round(lowestyear[1], 1)),
-        help = 'By average of winter min temp'
+        delta= 'num: '+ str(round(lowestyear[1], 1)),
+        help = 'Year of lowest number of tropical nights'
     )
 
-    highestyear = df_filtered_cityonly[(df_filtered_cityonly['month'].isin([6, 7, 8]))].groupby(by = 'year').mean().reset_index()
-    highestyear = highestyear.sort_values(by = 'max', ascending = False)[['year', 'max']].iloc[0, :]
+
+    highestyear = tropical_filtered_cityonly.sort_values(by = 'data', ascending = False)[['year', 'data']].iloc[0,:]
+
     # st.write(highestyear)
 
     kpi4.metric(
         label="Warmest year 🥵",
         value= highestyear[0],
-        delta= 'temp: '+ str(round(highestyear[1], 1)),
-        help = 'By average of summer max temp'
+        delta= 'num: '+ str(round(highestyear[1], 1)),
+        help = 'Year of highest number of tropical nights'
     )
+
+
+# map
+st.markdown('#')
+
+
+
+
+@st.cache
+def getmap(data,col='data'):
+    if False:
+        fig=px.choropleth_mapbox(data,
+                                 geojson=geojson,
+                                 locations='location',
+                                 color = col,
+                                 mapbox_style='carto-positron',
+                                 color_continuous_scale="Reds",
+                                 range_color=[0,40],
+                                 animation_frame='year',
+
+                                 center = {'lat':35.757981,'lon':127.661132},
+                                 zoom=5.5,
+                                 labels='data'
+                                 )
+
+    else:
+        fig=px.choropleth_mapbox(data,
+                                 geojson=geojson,
+                                 locations='location',
+                                 color = col,
+                                 mapbox_style='carto-positron',
+                                 color_continuous_scale="Reds",
+                                 range_color=[0,40],
+                                 # animation_frame='year',
+
+                                 center = {'lat':35.757981,'lon':127.661132},
+                                 zoom=5.5,
+                                 labels='data'
+                                 )
+    fig.update_layout(margin={"r":0,"l":0,"t":0,"b":0})
+    return fig
+
+def animation(speed = 0.1):
+    hist = pd.Series()
+    histfig,hax = plt.subplots()
+    for year,data in gb:
+        hax.clear()
+        mdf = to_map_df(data,datacol = ['data'])
+        hist.loc[year] = df[(df['location']=='전국') & (df['year']== year)]['data'].iloc[0]
+        # 지도 그리기
+        mapfig=getmap(mdf)
+        hist.plot(ax = hax, color='black')
+        with label:
+            st.text(year)
+        with e1:
+            c1,c2 = st.columns(2)
+            with c1:
+                st.plotly_chart(mapfig)
+            with c2:
+                st.pyplot(histfig)
+
+        time.sleep(speed)
+
+df = pd.read_csv('total.csv')
+
+if __name__ == "__main__":
+    # load all data
+    res= df
+    gb = res.groupby('year')
+    years = list(res.year.values.astype(int))
+
+    # load geojson
+    geojson = json.load(open('korea_geojson2.geojson',encoding='utf-8'))
+    ids=[]
+    for x in geojson['features']:
+        id = x['properties']['CTP_KOR_NM']
+        x['id'] = id
+        ids.append(id)
+    ids = list(set(ids))
+
+
+    with st.container():
+        # year slider
+        year = st.slider("year",1973,2022)
+        temp = gb.get_group(year)
+
+        # plot
+        label = st.empty()
+        e1 = st.empty()
+        e2 = st.empty()
+
+
+        #mdf = to_map_df(res.groupby(['year','location']).mean().reset_index(), datacol = ['avg','year'])
+        mdf = to_map_df(temp,datacol=['data'])
+        hist = gb.sum()['data'].loc[:year]
+
+        # 지도 그리기
+        histfig,hax = plt.subplots()
+        mapfig = getmap(mdf,col='data')
+        hist.plot(ax = hax,color = 'black')
+
+        with label:
+            st.text(year)
+        with e1:
+            c1,c2 = st.columns(2)
+            with c1:
+                st.plotly_chart(mapfig, use_container_width=True)
+            with c2:
+                st.pyplot(histfig)
+    
+
+        st.button("Play",on_click=animation)
+
+
 
 # Stats 선택 & 폭염 선택
 elif (selected != 'Home') & (add_topic == '폭염'):
