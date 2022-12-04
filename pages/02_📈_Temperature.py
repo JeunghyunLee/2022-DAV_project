0e6c00fe837b4ebf9efc15531960429ca218f36a
Lethe <smo304@nyu.edu>
2022-12-04 21:23:07 +0900

    fixing errors

--- pages/02_📈_Temperature.py
+++ pages/02_📈_Temperature.py
@@ -26,7 +26,6 @@ with st.sidebar:
 
 @st.cache
 def loaddata():
-    areas = ['강원영동','강원영서','경남','경북','서울경기','전남','전북','충남','충북','제주']
     res = pd.DataFrame()
     for area in areas:
         df = pd.read_csv(path + "%s.csv"%area)
@@ -68,12 +67,10 @@ def animation(speed = 0.01):
 
 
 # Load files
-names = ['전국', '강원영동', '강원영서', '경남', '경북',
-        '서울경기', '전남', '전북', '제주', '충남', '충북']
 df = pd.DataFrame()
-for name in areas :
-    temp = pd.read_csv(path +str(name) + '.csv')
-    temp['지역'] = name
+for area in areas :
+    temp = pd.read_csv(path +str(area) + '.csv')
+    temp['지역'] = area
     df = pd.concat([df, temp], axis = 0)
 
 df = df.dropna()
@@ -85,9 +82,6 @@ df['month'] = df['date'].dt.month
 st.markdown(
         '''### :thermometer: Temperature''')
 
-# 사이드바
-with st.sidebar:
-    region_filter = st.selectbox("Select the City", pd.unique(df["지역"]))
 
     
 st.write('### Geographical Statistics')
@@ -99,7 +93,6 @@ with st.container():
     with st.container():
         # year slider
         year = st.slider('Select Year',min(years),max(years), value=max(years))
-        st.write('Selected Year:', year)
         temp = gb.get_group(year)
 
         # plot
@@ -123,19 +116,6 @@ with st.container():
                 plt.title("Average Temperature")
                 st.pyplot(histfig)
         button = st.button("Play",on_click=animation)
-         with label:
-            st.text(year)
-            # year = 2022
-        with e1.container():
-            c1,c2 = st.columns(2)
-            with c1:
-                mapfig = getmap(mdf, col='avg')
-                st.plotly_chart(mapfig, use_container_width = True)
-                
-            with c2:
-                hist.plot(ax = hax,color = 'black')
-                plt.title("Average Temperature")
-                st.pyplot(histfig)
 
 
 st.markdown("""---""")
