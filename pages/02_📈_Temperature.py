c749c8876909dd92e094282fa2fb7610c5f70b1a
Lethe <smo304@nyu.edu>
2022-12-04 21:38:03 +0900

    fixing some errors

--- pages/02_📈_Temperature.py
+++ pages/02_📈_Temperature.py
@@ -70,7 +70,7 @@ def animation(speed = 0.01):
 df = pd.DataFrame()
 for area in areas :
     temp = pd.read_csv(path +str(area) + '.csv')
-    temp['지역'] = area
+    temp['location'] = area
     df = pd.concat([df, temp], axis = 0)
 
 df = df.dropna()
@@ -81,10 +81,6 @@ df['month'] = df['date'].dt.month
 # 상단 제목
 st.markdown(
         '''### :thermometer: Temperature''')
-
-
-    
-st.write('### Geographical Statistics')
 with st.container():
     # load all data
     res=loaddata()
@@ -118,14 +114,13 @@ with st.container():
         button = st.button("Play",on_click=animation)
 
 
-st.markdown("""---""")
-st.write('### Region Statistics _ {}'.format(region))
 with st.container():
+    st.markdown("""---""")
+    st.write('### Region Statistics _ {}'.format(region))
     kpi1, kpi2, kpi3, kpi4 = st.columns(4)
 
     # 선택한 지역, 연도 filter
-    df_filtered = df[(df['지역'] == region) ]
-    df_filtered_cityonly = df[(df['지역'] == region)]
+    df_filtered = df[(df['location'] == region) ]
 
     kpi1.metric(
         label=f"now at",
@@ -133,7 +128,7 @@ with st.container():
     )
 
     kpi2.metric(
-        label=f"Summer, Winter average temperature",
+        label=f"Winter, Summer average temperature",
         value=str(round(
             df_filtered[df_filtered['month'].isin([12, 1, 2])]
             ['avg'].mean()
@@ -144,7 +139,7 @@ with st.container():
     )
 
 
-    highestyear = df_filtered_cityonly[(df_filtered_cityonly['month'].isin([6, 7, 8]))].groupby(by = 'year').mean().reset_index()
+    highestyear = df_filtered[(df_filtered['month'].isin([6, 7, 8]))].groupby(by = 'year').mean().reset_index()
     highestyear = highestyear.sort_values(by = 'max', ascending = False)[['year', 'max']].iloc[0, :]
     # st.write(highestyear)
 
@@ -155,7 +150,7 @@ with st.container():
         help = 'By average of summer max temp'
     )
 
-    lowestyear = df_filtered_cityonly[(df_filtered_cityonly['month'].isin([12, 1, 2]))].groupby(by = 'year').mean().reset_index()
+    lowestyear = df_filtered[(df_filtered['month'].isin([12, 1, 2]))].groupby(by = 'year').mean().reset_index()
     lowestyear = lowestyear.sort_values(by = 'min', ascending = True)[['year', 'min']].iloc[0,:]
 
     kpi4.metric(
@@ -167,7 +162,7 @@ with st.container():
 
     fig_col3, fig_col4  = st.columns(2)
 
-    densityyear = df_filtered_cityonly.groupby(by = ['year']).mean().reset_index()[['year', 'max']]
+    densityyear = df_filtered.groupby(by = ['year']).mean().reset_index()[['year', 'max']]
     densityyear = densityyear.transpose()
     densityyear.columns = range(1973, 2023)
     # st.write(densityyear)
@@ -185,31 +180,32 @@ with st.container():
         )
 
         st.plotly_chart(fig_year,use_container_width = True )
-    
 
-st.markdown("""---""")
-st.markdown('#### Related Contents')
-st.markdown('''
-    If you are interested in learning more informations about climate change in South Korea here are few references that you might want to check out
-    * Documents
-        * 기상청, 한국 기후변화 평가 보고서 2020 
-            * http://www.climate.go.kr/home/cc_data/2020/Korean_Climate_Change_Assessment_Report_2020_2.pdf
-        * IPCC Climate Change 2022
-            * https://www.ipcc.ch/report/ar6/wg3/downloads/report/IPCC_AR6_WGIII_SPM.pdf
-    * Books
-        * 이동학, 쓰레기책:왜 지구의 절반은 쓰레기도 뒤덮이는가
-        * 김백민, 우리는 결국 지구를 위한 답을 찾을 것이다
-        * 박훈, 지속가능한 미래를 위한 기후변화 데이터북 
-    ''')
-st.markdown("""---""")
-fig_col1, fig_col2 = st.columns(2)
-with fig_col1 :
-    # Embed a youtube video
-    st.markdown('##### Netflix: See what three degrees of global warming looks like')
-    st_player("https://youtu.be/cTQ3Ko9ZKg8")
-    
-with fig_col2 : 
-    # Embed a youtube video
-    st.markdown('##### Economist: See what three degrees of global warming looks like')
-    st_player("https://youtu.be/uynhvHZUOOo")
+# misc contents
+with st.container():
+    st.markdown("""---""")
+    st.markdown('#### Related Contents')
+    st.markdown('''
+        If you are interested in learning more informations about climate change in South Korea here are few references that you might want to check out
+        * Documents
+            * 기상청, 한국 기후변화 평가 보고서 2020 
+                * http://www.climate.go.kr/home/cc_data/2020/Korean_Climate_Change_Assessment_Report_2020_2.pdf
+            * IPCC Climate Change 2022
+                * https://www.ipcc.ch/report/ar6/wg3/downloads/report/IPCC_AR6_WGIII_SPM.pdf
+        * Books
+            * 이동학, 쓰레기책:왜 지구의 절반은 쓰레기도 뒤덮이는가
+            * 김백민, 우리는 결국 지구를 위한 답을 찾을 것이다
+            * 박훈, 지속가능한 미래를 위한 기후변화 데이터북 
+        ''')
+    st.markdown("""---""")
+    fig_col1, fig_col2 = st.columns(2)
+    with fig_col1 :
+        # Embed a youtube video
+        st.markdown('##### Netflix: See what three degrees of global warming looks like')
+        st_player("https://youtu.be/cTQ3Ko9ZKg8")
+        
+    with fig_col2 : 
+        # Embed a youtube video
+        st.markdown('##### Economist: See what three degrees of global warming looks like')
+        st_player("https://youtu.be/uynhvHZUOOo")
 
