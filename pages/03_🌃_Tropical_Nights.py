d1a2c9261de9cecbc0ebc6c633ccfa0852c2d84e
Lethe <smo304@nyu.edu>
2022-12-05 21:54:34 +0900

    cleaning up data for tropical

--- pages/03_🌃_Tropical_Nights.py
+++ pages/03_🌃_Tropical_Nights.py
@@ -9,6 +9,17 @@ rng = (0,25)
 with st.sidebar:
     region = st.selectbox("도시를 선택해주세요", areas)
 
+@st.cache
+def loaddata():
+    res = pd.DataFrame()
+    for area in areas:
+        df = pd.read_csv("data_tropical/%s.csv"%area)
+        df = df.dropna()
+        df['location']=area
+        res = pd.concat([res,df])
+    res=res.reset_index()
+    return res
+
 
 def animation(speed = 0.1):
     hist = pd.Series()
@@ -33,9 +44,8 @@ def animation(speed = 0.1):
 
 
 # load all data
-res= pd.read_csv('data_tropical/total.csv')
+res= loaddata()
 gb = res.groupby('year')
-
 # 상단 제목
 st.markdown(
         '''### :night_with_stars: 열대야''')
@@ -76,14 +86,12 @@ st.markdown(
         ''':bulb: 화면 좌측의 탭에서 지역을 선택해 주세요.''')
 st.write('### {} 지역의 열대야 통계'.format(region))
 with st.container():
-    df2 = pd.read_csv('data_tropical/total3.csv')
-
+    
     ## region_selectbox
-    df2 = df2[df2['지역'] == region]
-    df2.drop(['Unnamed: 0'], axis = 1, inplace = True)
+    df2 = res[res['location'] == region]
 
     ## line_chart animation
-    fig2 = px.line(df2, x='연도', y='연합계', color='지역')    
+    fig2 = px.line(df2, x='year', y='data', color='location')    
     st.plotly_chart(fig2, use_container_width=True)
 
 
