245585eb51556535640eef798e681cbdcbe1c9bf
JeunghyunLee <jeunghyunleee@gmail.com>
2022-12-06 14:30:03 +0900

    interim update of main page

--- 01_🌏_Main_Page.py
+++ 01_🌏_Main_Page.py
@@ -12,12 +12,14 @@ st.set_page_config(
     }
 )
 
-expander = st.sidebar.expander("What is this page?")
+expander = st.sidebar.expander("What is this page about?")
 expander.write(
     """
-이 페이지는 **한국의 기후 변화**에 대한 정보를 데이터 시각화를 통해 효과적으로 전달하고자 만들어졌습니다.\\
-1973년부터 2022년까지 전국, 서울경기, 강원도, 경북, 경남, 전북, 전남, 충북, 충남, 제주의
-기온, 열대야 일수, 폭염 일수, 강수량 데이터를 사용하고 있습니다. \\
+이 페이지는 **한국의 기후 변화**에 대한 정보를 데이터 시각화를 통해 효과적으로 전달하고자 만들어졌습니다.
+
+**1973년**부터 **2022년**까지 전국, 서울경기, 강원도, 경북, 경남, 전북, 전남, 충북, 충남, 제주의
+**기온, 열대야 일수, 폭염 일수, 강수량** 데이터를 사용하고 있습니다.
+
 페이지를 이동하면서 궁금한 내용들을 살펴보세요.
 """
 )
@@ -25,8 +27,12 @@ expander.write(
 st.image("./melting_earth.png", width=130)
 st.markdown('# 녹는 아이스크림, 지구맛 출시 :droplet:')
 # st.markdown("This page is a Streamlit dashboard that can be used to explore the statistics about climate change in South Korea over the past 50 years.")
-st.markdown("#### 들어가며 ♟")
-st.markdown("본 페이지는 ___를 위해 제작되었습니다.")
+st.markdown("#### 들어가며")
+st.markdown("우리나라는 최근, 100년 전에 비해 **여름이 20일** 길어지고 **겨울이 22일** 짧아졌다고 합니다. 봄은 17일 정도, 여름은 11일 정도 빨리 시작되고 있습니다.")
+st.markdown('''>* 최근 30년(1991\~2020년)은 과거 30년(1912\~1940)년에 비해 **연평균기온이 1.6℃ 상승**하였습니다.
+>* 출처: [*2021년 기후변화 보고서, 기상청 기후과학국*](https://www.kma.go.kr/kma/servlet/NeoboardProcess?mode=download&bid=press&num=1194009&fno=2&callback=https%3A%2F%2Fwww.kma.go.kr%2Fkma%2Fnews%2Fpress.jsp&ses=USERSESSION&k=ATC202104271102112_053690f3-20d6-4005-b700-009a4e1fd9da.pdf)
+''')
+st.markdown("다시 한 번 기후 위기의 심각성을 체감하고, *2030년 온실가스 감축*이라는 목표와, *2050년 탄소중립*까지 숨가쁘게 달려가는 데에 기여할 수 있기를 기대합니다. ")
 st.markdown("#### 사용한 데이터")
 st.markdown('''
         * 기상청 [기상자료개방포털](https://data.kma.go.kr/stcs/grnd/grndTaList.do?pgmNo=70)
